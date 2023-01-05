d68daf0a5eee06c76cadf317565b3026f9fc71fa
Umar-Gonzales <121957535+Umar-Gonzales@users.noreply.github.com>
2023-01-05 17:34:42 +0200

    create todo list

---
+++ precache-manifest.4b6ed7bd9e3224f0441bf07fc42e598f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1faa49957b1e929a60158accd225a5fe",
+    "url": "/Andrew01011997/todo-list.git/index.html"
+  },
+  {
+    "revision": "4a294bda83aba39ccce8",
+    "url": "/Andrew01011997/todo-list.git/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "4a294bda83aba39ccce8",
+    "url": "/Andrew01011997/todo-list.git/static/js/2.07c0cde6.chunk.js"
+  },
+  {
+    "revision": "2faee23d13ca47731871",
+    "url": "/Andrew01011997/todo-list.git/static/js/main.3ca0603e.chunk.js"
+  },
+  {
+    "revision": "d7021c8b3f16c9396613",
+    "url": "/Andrew01011997/todo-list.git/static/js/runtime~main.0f9b069b.js"
+  }
+]);
